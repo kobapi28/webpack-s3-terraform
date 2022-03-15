a53d051c5ed85d2331bb126f31b22008d3262242
Shoma Kobayashi <sho1228.k@gmail.com>
2022-03-15 13:52:17 +0900

    [ci] add assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,5 +6,13 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { "minScore": 90 }],
+        "categories:accessibility": ["error", { "minScore": 90 }],
+        "categories:best-practices": ["error", { "minScore": 90 }],
+        "categories:seo": ["error", { "minScore": 90 }]
+      }
+    }
   },
 };
